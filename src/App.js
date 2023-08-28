33d655a015c2899c945c2f487abf66d2553b180b
Pirmin Pernsteiner <98333136+PirminP@users.noreply.github.com>
2023-08-28 17:07:46 -0300

    update: All files to implement css styling

--- src/App.js
+++ src/App.js
@@ -1,36 +1,46 @@
 import React from 'react';
 import './App.css';
-import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import { Route, Switch, BrowserRouter, Link } from 'react-router-dom';
 import Search from './pages/Search';
 import Carrinho from './pages/Carrinho';
 import ProductDetails from './pages/ProductDetails';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
+    <div className="App">
+      <BrowserRouter>
+        <header className="app-header">
+          <h2><Link to="/">Frontend Online Store</Link></h2>
+          <nav className="app-header-nav">
+            <Link to="/">Início</Link>
+            <hr />
+            <Link to="/carrinho">Carrinho</Link>
+          </nav>
+        </header>
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={ (props) => (
+              <Search
+                { ...props }
+              />
+            ) }
+          />
+          <Route
+            exact
+            path="/carrinho"
+            render={ (props) => <Carrinho { ...props } /> }
+          />
+          <Route
+            exact
+            path="/product-details/:id"
+            render={ (props) => <ProductDetails { ...props } /> }
+          />
+        </Switch>
+      </BrowserRouter>
+    </div>
 
-        <Route
-          exact
-          path="/"
-          render={ (props) => (
-            <Search
-              { ...props }
-            />
-          ) }
-        />
-        <Route
-          exact
-          path="/carrinho"
-          render={ (props) => <Carrinho { ...props } /> }
-        />
-        <Route
-          exact
-          path="/product-details/:id"
-          render={ (props) => <ProductDetails { ...props } /> }
-        />
-      </Switch>
-    </BrowserRouter>
   );
 }
 
